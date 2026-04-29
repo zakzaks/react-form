4aedefd6c37e2430ae3ed397b35f08e90f0bb980
zakzaks <ariefn82@gmail.com>
2026-04-29 20:53:12 +0700

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+export default function Login() {
+	const [enteredValue, setEnteredValue] = useState({
+		email: "",
+		password: "",
+	});
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		console.log(enteredValue);
+	}
+
+	function handleInputChange(identifier, value) {
+		setEnteredValue((prev) => ({
+			...prev,
+			[identifier]: value,
+		}));
+	}
+
+	return (
+		<form>
+			<h2>Login</h2>
+
+			<div className="control-row">
+				<div className="control no-margin">
+					<label htmlFor="email">Email</label>
+					<input
+						id="email"
+						type="email"
+						name="email"
+						onChange={(event) => handleInputChange("email", event.target.value)}
+						value={enteredValue.email}
+					/>
+				</div>
+
+				<div className="control no-margin">
+					<label htmlFor="password">Password</label>
+					<input
+						id="password"
+						type="password"
+						name="password"
+						onChange={(event) =>
+							handleInputChange("password", event.target.value)
+						}
+						value={enteredValue.password}
+					/>
+				</div>
+			</div>
+
+			<p className="form-actions">
+				<button className="button button-flat">Reset</button>
+				<button className="button" onClick={handleSubmit}>
+					Login
+				</button>
+			</p>
+		</form>
+	);
+}
